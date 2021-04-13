b3f3c3f2dc0fdcdc68ae7fe95d60133830b5fe55
Evan Higgins <evdhiggins@gmail.com>
2021-04-12 21:21:04 -0500

    Load port from .env; add basic credential parsing

--- src/Main.hs
+++ src/Main.hs
@@ -3,28 +3,56 @@
 module Main where
 
 import           Configuration.Dotenv ( loadFile, defaultConfig )
+import           Control.Exception ( Exception, throw )
 import           Control.Monad ( void, mapM_ )
 import qualified Data.ByteString            as B
+import           Data.Char ( isSpace )
+import           Data.Data ( Typeable )
 import qualified Data.Text                  as T
 import           Network.FTP.Client ( RTypeCode (TA) )
 import qualified Network.FTP.Client         as FTP
 import qualified System.Directory           as D
-import           System.Environment ( getEnv )
+import           System.Environment ( lookupEnv )
+import           Text.Read ( readMaybe )
 
-data Credentials = Credentials { user :: String, pass :: String, host :: String }
+data Credentials = Credentials { user :: String, pass :: String, host :: String, port :: Int }
+
+newtype CredentialsException = CredentialsException String
+    deriving (Show, Typeable)
+
+instance Exception CredentialsException
 
 getCredentials :: IO Credentials
 getCredentials = do
-    void $ loadFile defaultConfig
-    Credentials <$> getEnv "FTP_USER"
-                <*> getEnv "FTP_PASS"
-                <*> getEnv "FTP_HOST"
+    _ <- loadFile defaultConfig
+    u <- nonEmptyString <$> lookupEnv "FTP_USER"
+    p <- nonEmptyString <$> lookupEnv "FTP_PASS"
+    h <- nonEmptyString <$> lookupEnv "FTP_HOST"
+    pt <- parsePort <$> lookupEnv "FTP_PORT"
+
+    case Credentials <$> u <*> p <*> h <*> pt of
+        Just c -> return c
+        _ -> throw $ CredentialsException "Invalid FTP credentials"
+
+nonEmptyString :: Maybe String -> Maybe String
+nonEmptyString s = case dropWhile isSpace <$> s of
+    Just [] -> Nothing
+    Just s -> Just s
+    _ -> Nothing
+
+parsePort :: Maybe String -> Maybe Int
+parsePort Nothing = Nothing
+parsePort (Just p) = case readMaybe p :: Maybe Int of
+    Just pt
+        | pt > 0 -> Just pt
+        | otherwise -> Nothing
+    _ -> Nothing
 
 main :: IO ()
 main = do
     putStrLn ""
-    Credentials { user, pass, host } <- getCredentials
-    handleFtp user pass host
+    Credentials { user, pass, host, port } <- getCredentials
+    handleFtp user pass host port
 
 isTextFile :: FilePath -> Bool
 isTextFile f = (==) ".txt" $ T.takeEnd 4 $ T.pack f
@@ -32,8 +60,8 @@ isTextFile f = (==) ".txt" $ T.takeEnd 4 $ T.pack f
 txtFilePaths :: IO [FilePath]
 txtFilePaths = filter isTextFile <$> D.listDirectory "."
 
-handleFtp :: String -> String -> String -> IO ()
-handleFtp u p host = FTP.withFTP host 5021 $ \h welcome -> do
+handleFtp :: String -> String -> String -> Int -> IO ()
+handleFtp u p host pt = FTP.withFTP host pt $ \h welcome -> do
     initFtp h u p
     tp <- txtFilePaths
     putStrLn $ "Found " ++ show (length tp) ++ " file(s) to upload"
